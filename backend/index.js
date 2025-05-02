2aaf801830dea297092ebbc9eacb8d3cfb5e56d8
Károlyi Krisztián <145534392+krisztiankarolyi@users.noreply.github.com>
2025-05-02 15:03:05 +0300

    Update index.js
    CORS options

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,12 @@ const pool = new pg.Pool({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: 'https://contacts-app-frontend-j8kd.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 app.use('/uploads', cors(), express.static('uploads'));
 app.use('/avatars', cors(),  express.static('uploads'));
 
